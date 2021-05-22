0e223f2ee3fa3dd32e293edac3bc158a226af90c
William Johnson <wcjohnson@oigroup.net>
2021-05-22 05:21:45 -0700

    feat: allow App to be used as a module alias for the app source dir

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -334,6 +334,8 @@ module.exports = function (webpackEnv) {
           'scheduler/tracing': 'scheduler/tracing-profiling',
         }),
         ...(modules.webpackAliases || {}),
+        // Allow App to be used as an alias for the app source directory
+        'App': path.resolve(__dirname, "../src"),
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
